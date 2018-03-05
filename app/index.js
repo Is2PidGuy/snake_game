1ca3a9318def9cbbf0dd75aaec1cf1a6b332df25
Is2PidGuy <stoopidguy1992@gmail.com>
2018-03-05 23:52:58 +0545

    added texturing

--- app/index.js
+++ app/index.js
@@ -3,6 +3,9 @@ import {
     select,
 } from 'd3';
 
+import snake from './snake.jpg';
+import { easeBack } from 'd3-ease';
+
 // create N points such that adjacent nodes are L distance apart
 const createPoints = (N, L) => {
     let x = 100;
@@ -35,8 +38,8 @@ const satisfyLinkConstraints = (points, L) => {
 
 class Main extends React.Component {
     componentDidMount() {
-        this.N = 280;
-        this.L = 2;
+        this.N = 40;
+        this.L = 10;
         this.points = createPoints(this.N, this.L);
         this.draw();
         this.headAngle = -Math.PI / 2;
@@ -87,22 +90,62 @@ class Main extends React.Component {
     draw() {
         const node = select(this.node);
         node.style('position', 'relative');
-        const updateSel = node.selectAll('div').data(this.points);
-        const headWidth = 30;
+        // select immediate divs inside node
+        const updateSel = node.selectAll(':scope > div').data(this.points);
+        const headWidth = 40;
         const tailWidth = 10;
-        const width = (index) => 
-            headWidth + (tailWidth - headWidth) * (index / (this.N - 1));
-        updateSel.enter()
+        const noOfChunksForHead = 4;
+        const width = (index) => {
+            if (index < noOfChunksForHead) {
+                const peakWidth = 30;
+                const bigWidth = 50;
+                const f = index / (noOfChunksForHead - 1);
+                return peakWidth + f * (bigWidth - peakWidth);
+            }
+            return headWidth + (tailWidth - headWidth) * (index / (this.N - 1));
+        }
+        const sel = updateSel.enter()
             .append('div')
             .style('position', 'absolute')
-            .style('background', 'red')
+            .style('background', `url(${snake})`)
             .style('border-radius', '50%')
             .style('width', (d, i) => `${width(i)}px`)
             .style('height', (d, i) => `${width(i)}px`)
-            .merge(updateSel)
-            .style('left', d => `${d.x}px`)
-            .style('top', d => `${d.y}px`)
-            ;
+            .merge(updateSel);
+        sel
+            .style('left', (d, i) => `${d.x - width(i) / 2}px`)
+            .style('top', (d, i) => `${d.y - width(i) / 2}px`);
+
+        const fp = this.points[0];
+            sel.each(function (d, i) {
+                if (i !== 2) return;
+                const ang = Math.atan2(fp.y - d.y, fp.x - d.x) + Math.PI / 2;
+                const nd = select(this);
+                const w = 8;
+                nd.style('z-index', 99);
+                nd.html('');
+                nd.style('transform', `rotateZ(${ang}rad)`);
+
+                nd.append('div')
+                .style('position', 'absolute')
+                .style('background', 'black')
+                .style('border', '3px solid white')
+                .style('border-radius', '50%')
+                .style('left', '10%')
+                .style('top', '10%')
+                .style('width', `${w}px`)
+                .style('height', `${w}px`);
+
+                nd.append('div')
+                .style('position', 'absolute')
+                .style('background', 'black')
+                .style('border', '3px solid white')
+                .style('border-radius', '50%')
+                .style('left', '60%')
+                .style('top', '10%')
+                .style('width', `${w}px`)
+                .style('height', `${w}px`);
+            });
     }
 
     render() {
--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,14 @@ module.exports = {
                 use: {
                     loader: 'babel-loader'
                 }
+            }, {
+                test: /\.(png|jpg|gif)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {}
+                    }
+                ]
             }
         ]
     }
